67cde5d87f10c85bf04b023451d7c4ef0d4ed715
donguyenphu <donguyenphu022008@gmail.com>
2025-02-15 11:50:38 +0700

    update preloader

---
+++ js/loader.js
@@ -0,0 +1,6 @@
+let loader = document.getElementById("preloader");
+window.addEventListener('load', function() {
+    setTimeout(function() {
+        loader.style.display = 'none';
+    }, 1000); 
+});
